d7f4e784ff90de9f810c6a8a682d51aee0bd8465
daiki <morilr.tabletennis@gmail.com>
2020-11-08 10:14:58 +0900

    プロフィール画像を変更できるようにした(edit,update)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,26 @@
 class ProfilesController < ApplicationController
   def show
     @user = current_user
+    @profile = current_user.profile
+  end
+
+  def edit
+    @profle = current_user.prepare_profile
+  end
+
+  def update
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save 
+      redirect_to profile_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:avatar)
   end
 end
+
