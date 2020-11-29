71f8515ec8ffeac53b569663342f58aa9400f58d
daiki <morilr.tabletennis@gmail.com>
2020-11-30 07:15:05 +0900

    コメントを変更

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -1,66 +1,87 @@
 import $ from 'jquery'
 
-// 矢印の表示
-const toggleChangeBtn = () => {
-  let slideIndex = $('.article-image').index($('.active'))
-  if(slideIndex == -1){
-    $('.slider-btn-left').hide()
-  }else if(slideIndex == $('.article-image').length - 1){
-    $('.slider-btn-right').hide()
-  }else{
-    $('.slider-btn-left').show()
-    $('.slider-btn-right').show()
-  }
-}
+
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
+
   $('.article-image:nth-of-type(n + 4)').addClass('hidden')
-  toggleChangeBtn();
 
-  let clicks = 0;
-  const $fourthImage = $('.article-image:nth-of-type(4)')
-  
-  
-
-  // 右ボタンを押した時の動作
-  $(document).on('click','.slider-btn-right', () => {
-    let activeIndex = $('.article-image').index($('.active'))
-    if(activeIndex == -1){
-      $('.article-image:nth-of-type(-n + 3)').addClass('hidden')
-      $fourthImage.removeClass('hidden')
-      $fourthImage.addClass('active')
-    }else{
-      let $active = $('.active')
-      $active.removeClass('active')
-      $active.addClass('hidden')
-      $active.next().addClass('active')
-      $active.next().removeClass('hidden') 
+  // 別々で処理を行う
+  $('.user-articles-box').each((i,val) => {
+    // 矢印の表示
+    const toggleChangeBtn = () => {
+      let activeIndex = $(val).find('.article-image').index($('.active'))
+      let imageIndex = $(val).find('.article-image').length
+      if(imageIndex <= 3){
+        // 画像が3枚以下の場合
+        $(val).find('.slider-btn-left').hide()
+        $(val).find('.slider-btn-right').hide()
+      }else if(activeIndex == -1){
+        // 最初の３枚が表示されているとき
+        $(val).find('.slider-btn-left').hide()
+        $(val).find('.slider-btn-right').show()
+      }else if(activeIndex == $(val).find('.article-image').length - 1){
+        // 画像が最後だったとき
+        $(val).find('.slider-btn-right').hide()
+        $(val).find('.slider-btn-left').show()
+      }else{
+        // 画像がまだ右にも左にも表示できる時
+        $(val).find('.slider-btn-left').show()
+        $(val).find('.slider-btn-right').show()
+      }
     }
+
     toggleChangeBtn();
+
+    // 次の画像に進むの処理
+    $(val).on('click', '.slider-btn-right', () => {
+      let activeIndex = $(val).find('.article-image').index($('.active'))
+      let $fourthImage = $(val).find('.article-image:nth-of-type(4)')
+      if(activeIndex == -1){
+        // 最初の３枚が表示された時
+        $(val).find('.article-image:nth-of-type(-n + 3)').addClass('hidden')
+        $fourthImage.removeClass('hidden')
+        $fourthImage.addClass('active')
+      }else{
+        // 最初の3枚以外
+        let $active = $(val).find('.active')
+        $active.removeClass('active')
+        $active.addClass('hidden')
+        $active.next().addClass('active')
+        $active.next().removeClass('hidden') 
+      }
+      toggleChangeBtn();
+    })
+
+
+
+    // 前の画像に戻る処理
+    $(val).on('click','.slider-btn-left', () => {
+      let activeIndex = $(val).find('.article-image').index($('.active'))
+      let $active = $(val).find('.active')
+      if(activeIndex == 3) {
+        $(val).find('.article-image:nth-of-type(-n + 3)').removeClass('hidden')
+        $active.removeClass('active')
+        $active.addClass('hidden')
+      }else{
+        $active.removeClass('active')
+        $active.addClass('hidden')
+        $active.prev().addClass('active')
+        $active.prev().removeClass('hidden')
+      }
+      toggleChangeBtn();
+    })
+
+    
   })
 
-
-  // 左ボタンを押した時の動作
-  $(document).on('click','.slider-btn-left', () => {
-    let activeIndex = $('.article-image').index($('.active'))
-    let $active = $('.active')
-    if(activeIndex == 3) {
-      $('.article-image:nth-of-type(-n + 3)').removeClass('hidden')
-      $active.removeClass('active')
-      $active.addClass('hidden')
-    }else{
-      $active.removeClass('active')
-      $active.addClass('hidden')
-      $active.prev().addClass('active')
-      $active.prev().removeClass('hidden')
-    }
-    toggleChangeBtn();
-  })
- 
 
 })
+
+
+
 
 
 
