2e5a04130a4e3875450536f03d97b686f234dee4
daiki <morilr.tabletennis@gmail.com>
2020-12-01 07:55:31 +0900

    記事ごとに画像の切り替えを行えるようにした 画像３枚以下の時の表示方法の設定

--- app/javascript/packs/article.js
+++ app/javascript/packs/article.js
@@ -4,11 +4,26 @@ import $ from 'jquery'
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  // 画像が３枚以下だった時の処理
+  const imageCount = (val) => {
+    let imageIndex = $(val).find('.article-image').length
+    if(imageIndex == 1){
+      $(val).find('.article-image-box').removeClass('grid')
+      $(val).find('.article-image').addClass('one-image')
+    }else if(imageIndex == 2){
+      $(val).find('.article-image-box').removeClass('grid')
+      $(val).find('.img2').removeClass('article-image')
+      $(val).find('.article-image-box').addClass('two-image')
+    }
+  }
+
+  // 画像の４枚目以降を隠す
   $('.article-image:nth-of-type(n + 4)').addClass('hidden')
 
   // 矢印の表示
   const toggleChangeBtn = (val) => {
-    let activeIndex = $(val).find('.article-image').index($('.active'))
+    let index = $(val).find('.active')
+    let activeIndex = $(val).find('.article-image').index(index)
     let imageIndex = $(val).find('.article-image').length
     if(imageIndex <= 3){
       // 画像が3枚以下の場合
@@ -30,13 +45,15 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   //記事1つ1つに処理
-  $('.user-articles-box').each((i,val) => {
-    
+  $('.user-articles-box').each( (i,val) => {
+
+    imageCount(val);
     toggleChangeBtn(val);
 
     // 次の画像に進むの処理
     $(val).on('click', '.slider-btn-right', () => {
-      let activeIndex = $(val).find('.article-image').index($('.active'))
+      let index = $(val).find('.active')
+      let activeIndex = $(val).find('.article-image').index(index)
       let $fourthImage = $(val).find('.article-image:nth-of-type(4)')
       if(activeIndex == -1){
         // 最初の３枚が表示されている時
@@ -53,13 +70,12 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       toggleChangeBtn(val);
     })
-    
-  })
 
-  $('.user-articles-box').each((i,val) => {
+
     // 前の画像に戻る処理
     $(val).on('click','.slider-btn-left', () => {
-      let activeIndex = $(val).find('.article-image').index($('.active'))
+      let index = $(val).find('.active')
+      let activeIndex = $(val).find('.article-image').index(index)
       let $active = $(val).find('.active')
       if(activeIndex == 3) {
         // ４枚目が表示されている時
@@ -75,7 +91,9 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       toggleChangeBtn(val);
     })
+
   })
+
 })
 
 
