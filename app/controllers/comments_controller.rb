4489e49ddf9452041473495218a3bafd3f2a4015
daiki <morilr.tabletennis@gmail.com>
2020-12-03 10:07:58 +0900

    コメント機能実装

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,33 @@
+class CommentsController < ApplicationController
+
+  def index
+    article = Article.find(params[:article_id])
+    @comments = article.comments.all
+  end
+
+  def new
+    article = Article.find(params[:article_id])
+    @comment = article.comments.build(user_id: current_user.id)
+  end
+
+  def create
+    article = Article.find(params[:article_id])
+    @comment = article.comments.build(comment_params.merge(user_id: current_user.id))
+    if @comment.save 
+      redirect_to root_path, notice: 'コメントを作成しました'
+    else
+      flash.now[:error] = 'コメントを作成できませんでした'
+      render :new
+    end
+  end
+  
+  def destroy
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+  
+end
