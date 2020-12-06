5561512d6a22a419f81c3fad03fa62f69bf9b59c
daiki <morilr.tabletennis@gmail.com>
2020-12-06 14:55:45 +0900

    コメントを非同期で追加できるようにした

--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -8,21 +8,46 @@ axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 document.addEventListener('DOMContentLoaded', () => {
   let dataset = $('.comment-json').data()
   let articleId = dataset.articleId
-  console.log(articleId)
+  $('.comment-text-area-box').addClass('hidden')
   axios.get(`/articles/${articleId}/comments`)
     .then( (response) => {
       const comments = response.data
       comments.forEach( (comment) => {
-        console.log(comment)
         $('.comment-big-box').append(
           `<div class="comment-box"><div class="comment-user-box"><div class="comment-user-icon-box"><img src="${comment.avatar_url}" class= "comment-user-icon"></div><div class="comment-user-accountname-box">${comment.accountname}</div></div><div class="comment-content-box">${comment.content}</div></div>
           `)
-      
-
       })
-
-      
     })
+  $('.comment-form-box').on('click', () => {
+    $('.comment-form-box').addClass('hidden')
+    $('.comment-text-area-box').removeClass('hidden')
+    
+  })
+
+
+  $('.add-comment-btn').on('click', () => {
+    const content = $('#comment_content').val()
+    if (!content) {
+      window.alert('コメントを入力してください')
+    } else {
+      axios.post(`/articles/${articleId}/comments`, {
+        comment: {content: content}
+      })
+        .then( (res) => {
+          const comment = res.data
+          $('.comment-big-box').append(
+            `<div class="comment-box"><div class="comment-user-box"><div class="comment-user-icon-box"><img src="${comment.avatar_url}" class= "comment-user-icon"></div><div class="comment-user-accountname-box">${comment.accountname}</div></div><div class="comment-content-box">${comment.content}</div></div>
+            `)
+            $('#comment_content').val('')
+            $('.comment-text-area-box').addClass('hidden')
+            $('.comment-form-box').removeClass('hidden')
+        })
+
+    }
+  })
+
+
+
 })
   
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :articles do
     resource :like, only: [:show, :create, :destroy]
 
-    resources :comments, only: [:index, :new, :create, :destroy]
+    resources :comments, only: [:index, :create, :destroy]
   end
 
   namespace :api, defaults: {format: :json}  do
