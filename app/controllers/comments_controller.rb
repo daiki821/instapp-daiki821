1c2a342415b22451dcdeb21cc39763aad4252f39
daiki <morilr.tabletennis@gmail.com>
2020-12-06 12:04:57 +0900

    コメントをajaxで表示できるように設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,8 @@ class CommentsController < ApplicationController
   def index
     @article = Article.find(params[:article_id])
     @comments = @article.comments.all 
-    
+
+    render json: @comments, methods: [:avatar_url]
   end
 
   def new
--- app/javascript/packs/comment.js
+++ app/javascript/packs/comment.js
@@ -6,10 +6,25 @@ axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  axios.get(`/api/articles/3/comments`)
+  let dataset = $('.comment-json').data()
+  let articleId = dataset.articleId
+  console.log(articleId)
+  axios.get(`/articles/${articleId}/comments`)
     .then( (response) => {
-      console.log(response)
+      const comments = response.data
+      comments.forEach( (comment) => {
+        console.log(comment)
+        $('.comment-big-box').append(
+          `<div class="comment-box"><div class="comment-user-box"><div class="comment-user-icon-box"><img src="${comment.avatar_url}" class= "comment-user-icon"></div><div class="comment-user-accountname-box">${comment.accountname}</div></div><div class="comment-content-box">${comment.content}</div></div>
+          `)
+      
+
+      })
+
+      
     })
 })
   
+
+
 
